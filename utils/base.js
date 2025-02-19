39370d8a14290d2227175e7c0a40f01e1f63f79c
ZackaryW <gitzackw@gmail.com>
2025-02-18 22:45:53 -0800

    ref: make JsonCache singleton

--- utils/base.js
+++ utils/base.js
@@ -50,12 +50,21 @@ class fnCache {
  * JSON file caching system with file watching and automatic updates
  * @class
  * @param {string} path - Path to JSON file
- * @param {boolean} [changeListener=false] - Enable periodic change checking
- * @param {boolean} [touchFile=true] - Create file if it doesn't exist
+ * @param {Object} [options] - Configuration options
+ * @param {boolean} [options.changeListener=false] - Enable periodic change checking
+ * @param {boolean} [options.touchFile=true] - Create file if it doesn't exist
+ * @param {boolean} [options.autoSaveOnExit=true] - Automatically save data on process exit
  * @throws {Error} If file doesn't exist and touchFile is false
  */
 class JsonCache {
-    constructor(path, changeListener = false, touchFile = true) {
+    static #instances = new Map();
+
+    constructor(path, options = {}) {
+        if (JsonCache.#instances.has(path)) {
+            return JsonCache.#instances.get(path);
+        }
+
+        const { changeListener = false, touchFile = true, autoSaveOnExit = true } = options;
         this._path = path;
         this._changeListener = changeListener;
         this._cachedData = null;
@@ -74,6 +83,13 @@ class JsonCache {
                 this.updateCache();
             }
         });
+
+        // Auto-save on process exit
+        if (autoSaveOnExit) {
+            process.on('beforeExit', () => this.save());
+        }
+
+        JsonCache.#instances.set(path, this);
     }
 
     updateCache() {
